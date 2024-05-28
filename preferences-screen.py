b5bb73e576a4ae94f9c2185f83b4c385075e9492
mgrig11 <160203854+mgrig11@users.noreply.github.com>
2024-05-27 21:18:14 -0400

    Start preferences screen

---
+++ preferences-screen.py
@@ -0,0 +1,15 @@
+import json
+
+settings = {
+    "game_mode": "normal",  
+    "volume": {
+        "sfx": 50,  
+        "music": 50  
+    },
+    "keybinds": {
+        "move_up": "W",
+        "move_down": "S",
+        "move_left": "A",
+        "move_right": "D",
+    }
+}
